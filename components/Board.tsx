8171873e59b63c1d96f50ccebf2a0368e4b05302
Randy Flyte <877wrangler@gmail.com>
2023-08-28 20:02:01 -0400

    Stuck at CORS

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+'use client';
+
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { useEffect } from 'react';
+import { useBoardStore } from '@/store/BoardStore';
+
 
 export default function Board() {
+  
+  const getBoard = useBoardStore((state) => state.getBoard);
+
+  useEffect(() => {
+    getBoard();
+  }, [getBoard])
+  
   return (
-    <div>Board</div>
+    <h1>hello</h1>
+    // <DragDropContext>
+    //   <Droppable droppableId='board' direction='horizontal' type='column'>
+    //     {(provided) => <div>rendering all the columns</div>}
+    //   </Droppable>
+    // </DragDropContext>
   )
 }
