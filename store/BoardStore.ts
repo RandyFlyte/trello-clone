23d208c3bc81a0045f42a3d4073c11c99a140762
Randy Flyte <877wrangler@gmail.com>
2023-09-01 17:28:17 -0400

    Implement delete button for tasks

--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand'
 import { getTodosGroupedByColumn } from '@/lib/getTodosGroupedByColumn';
-import { databases } from '@/appwrite';
+import { databases, storage } from '@/appwrite';
 
 interface BoardState {
   board: Board;
@@ -8,10 +8,11 @@ interface BoardState {
   setBoardState: (board: Board) => void;
   updateTodoInDB: (todo: Todo, columnId: TypedColumn) => void;
   searchString: string,
-  setSearchString: (searchString: string) => void;
+  setSearchString: (searchString: string) => void,
+  deleteTask: (taskIndex: number, todoId: Todo, id: TypedColumn) => void,
 }
 
-export const useBoardStore = create<BoardState>((set) => ({
+export const useBoardStore = create<BoardState>((set, get) => ({
   board: {
     columns: new Map<TypedColumn, Column>()
   },
@@ -24,6 +25,25 @@ export const useBoardStore = create<BoardState>((set) => ({
     set({ board })
   },
   setBoardState: (board) => set({ board }),
+
+  deleteTask: async (taskIndex: number, todo: Todo, id: TypedColumn) => {
+    const newColumns = new Map(get().board.columns);
+
+    // Delete todoId from newColumns
+    newColumns.get(id)?.todos.splice(taskIndex, 1);
+
+    set({ board: { columns: newColumns } });
+
+    if (todo.image) {
+      await storage.deleteFile(todo.image.bucketId, todo.image.fileId);
+    }
+
+    await databases.deleteDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      todo.$id
+    )
+  },
 
   updateTodoInDB: async(todo, columnId) => {
     await databases.updateDocument(
